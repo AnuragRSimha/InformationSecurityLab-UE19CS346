a2f1bccd50821c0245e2b31bbbdb5d164c165182
Anurag.R.Simha <53004679+AnuragRSimha@users.noreply.github.com>
2022-05-08 22:37:05 +0530

    Add files via upload

---
+++ Week-1/LS.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<unistd.h>
+int main()
+{
+    printf("\n This is my ls Program\n");
+    printf("\n my real Uid is :%d\n My Effective uid is:%d\n", getuid(),geteuid());
+    return(0);
+}
